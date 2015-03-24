a926b6cb1cd4631d349fc6af8ea1e55ca57509db
erikamoriya <erikamoriya@Erikas-MacBook-Pro.local>
2015-03-24 14:17:08 -0300

    unit repos

---
+++ CadastroViewController.h
@@ -0,0 +1,13 @@
+//
+//  CadastroViewController.h
+//  CadastroUsuario
+//
+//  Created by Érika Tiemi Uehara Moriya on 3/24/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CadastroViewController : UIViewController
+
+@end
---
+++ CadastroViewController.m
@@ -0,0 +1,37 @@
+//
+//  CadastroViewController.m
+//  CadastroUsuario
+//
+//  Created by Érika Tiemi Uehara Moriya on 3/24/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "CadastroViewController.h"
+
+@interface CadastroViewController ()
+
+@end
+
+@implementation CadastroViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
